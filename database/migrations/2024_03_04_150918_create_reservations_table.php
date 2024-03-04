b17aa18c4ca4d6bf95fc7bb2271aa6faf4333651
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-04 22:46:22 +0100

    define migrations

--- database/migrations/2024_03_04_150918_create_reservations_table.php
+++ database/migrations/2024_03_04_150918_create_reservations_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_active')->default(true);
-            $table->rememberToken();
+            $table->foreignId('event_id')->constrained('events');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('status');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 };
