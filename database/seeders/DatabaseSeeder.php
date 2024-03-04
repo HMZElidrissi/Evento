9f36aca1164d5a035ce0bb663703af9b04f85ea8
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-04 23:01:26 +0100

    creating seeders + factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            CategorySeeder::class,
+            EventSeeder::class,
+            ReservationSeeder::class,
+        ]);
+        User::factory()->create([
+            'name' => 'Amine Ismaili',
+            'email' => 'amine@amine.com',
+            'password' => bcrypt('password'),
+            'role_id' => 1,
+        ]);
+        User::factory()->create([
+            'name' => 'Rania Bouabid',
+            'email' => 'rania@rania.com',
+            'password' => bcrypt('password'),
+            'role_id' => 2,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
